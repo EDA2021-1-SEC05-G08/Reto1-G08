234326fef52d6ed89a8c66f7e125a9572d1ada61
Sergio Franco <s.francop@uniandes.edu.co>
2021-09-10 19:23:04 -0500

    Avance de modelo

--- App/model.py
+++ App/model.py
@@ -37,6 +37,99 @@ los mismos.
 
 # Construccion de modelos
 
+def newCatalog_Artists():
+    """
+    Inicializa el catálogo de libros. Crea una lista vacia para guardar
+    todos los libros, adicionalmente, crea una lista vacia para los autores,
+    una lista vacia para los generos y una lista vacia para la asociación
+    generos y libros. Retorna el catalogo inicializado.
+    """
+    catalog_Artist = {'ConstituentID': None,
+               'DisplayName': None,
+               'ArtistBio': None,
+               'Nationality': None,
+               "Gender": None,
+               "BeginDate": None,
+               "EndDate": None,
+               "WikiQID": None,
+               "ULAN": None}
+
+    
+
+    catalog_Artist['ConstituentID'] = lt.newList()
+    catalog_Artist['DisplayName'] = lt.newList('SINGLE_LINKED')
+    catalog_Artist['ArtistBio'] = lt.newList('SINGLE_LINKED')
+    catalog_Artist['Nationality'] = lt.newList('SINGLE_LINKED')
+    catalog_Artist['Gender'] = lt.newList('SINGLE_LINKED')
+    catalog_Artist['BeginDate'] = lt.newList('SINGLE_LINKED')
+    catalog_Artist['EndDate'] = lt.newList('SINGLE_LINKED')
+    catalog_Artist['WikiQID'] = lt.newList('SINGLE_LINKED')
+    catalog_Artist['ULAN'] = lt.newList('SINGLE_LINKED')
+
+    return catalog_Artist
+
+def newCatalog_Artworks():
+    """
+    Inicializa el catálogo de libros. Crea una lista vacia para guardar
+    todos los libros, adicionalmente, crea una lista vacia para los autores,
+    una lista vacia para los generos y una lista vacia para la asociación
+    generos y libros. Retorna el catalogo inicializado.
+    """
+    catalog_Artworks = {'ObjectID': None,
+               'Title': None,
+               'ConstituentID': None,
+               'Date': None,
+               "Medium": None,
+               "Dimensions": None,
+               "CreditLine": None,
+               "AccesionNumber": None,
+               "Classification": None,
+               "Department": None,
+               "DateAcquired": None,
+               "Catalogued": None,
+               "URL": None,
+               "Circumference": None, 
+               "Depth": None,
+               "Diameter": None,
+               "Classification": None,
+               "Height": None,
+               "Length": None,
+               "Weight": None,
+               "Width": None,
+               "SeatHeight": None,
+               "Duration": None}
+
+    
+
+    catalog_Artworks["ObjectID"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Title"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["ConstituentID"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Date"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Medium"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Dimensions"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["CreditLine"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["AccesionNumber"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Classification"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Department"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["DataAcquiered"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Catalogued"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["URL"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Circumference"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Depth"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Diameter"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Classification"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Height"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Length"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Weight"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Width"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["SeatHeight"] = lt.newList('SINGLE_LINKED')
+    catalog_Artworks["Duration"] = lt.newList('SINGLE_LINKED')
+
+    return catalog_Artworks
+
+print(newCatalog_Artworks())
+
+
 # Funciones para agregar informacion al catalogo
 
 # Funciones para creacion de datos
